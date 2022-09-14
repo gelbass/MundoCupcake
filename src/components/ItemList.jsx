63d576aacbe67a86d0bbc70417f9b36ee7ee90f3
gelbass <lacruz.german@gmail.com>
2022-09-13 23:57:14 -0300

    1era entrega proyecto final

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import Item from "./Item";
 
 const ItemList = ({ productos , categoria}) => {
-	console.log(categoria);
 	const listadoProductos = categoria ? productos.filter(valor => valor.categoria === categoria) : productos;
-	console.log(listadoProductos);
 	return (
 		<div className="listProductos">
 			{listadoProductos.map(item => <Item key={item.id} id={item.id} nombre={item.nombre} urlImg={item.urlImg} stock={item.cantidad} precio={item.precio}/>)}
